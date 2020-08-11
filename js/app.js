10feadf71df6cef5b0e2d592a3938e896be67a25
stephon knight <YOUR EMAIL ADDRESS>
2020-08-11 16:28:04 -0700

    this is it

---
+++ js/app.js
@@ -0,0 +1,95 @@
+'use strict'
+
+console.log('customers')
+
+
+
+
+var storesHourArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
+
+var minArr = [23,3,11,20,2]
+var maxArr = [65,24,38,38,16]
+
+function salesOfCookies() {
+  for(var i=0; i < storesHourArr.length; i++)
+  var custPerHour = storesHourArr[i];
+  function custCount([minArr]){
+    for(var j = 0; j < 4 j++){
+      var custCount = [Math.ceil(Math.random() * (maxArr[j])
+      - minArr[j] + min)];
+      console.log(custCount)
+      return[custCount]; |
+    }
+
+
+  var custPerHour = custCount;
+console.log(custPerHour) , storesHourArr[i])
+return custPerHour;
+}
+
+}
+var seattle = {
+  miniCrust: 23,
+  maxCrust: 65,
+  avgCookieSale: 6.3,
+  custPerHour: function (){
+    numOfCust(this.miniCust,this.maxCrust)
+  }
+
+}
+
+for (var i = 0; i <= 13; i = i + 1){
+  var Toyoko = numofCust (3, 24);
+  console.log(storesHourArr[i],sales(Toyoko, 1.2));
+    storesHourArr[i], sales(Toyoko, 1.2)
+
+var Toyoko = {
+  miniCust: 3,
+  maxCrust: 24,
+  avgCookieSale: 1.2,
+  
+
+  
+}
+
+for (var i = 0; i <= 13; i = i + 1){
+  var Dubai = numofCust (11, 38);
+  console.log(storesHourArr[i],sales(Dubai, 3.7));
+    storesHourArr[i], sales(Dubai, 3.7)
+
+var Dubai = {
+  miniCrust: 11,
+  maxCrust:  38,
+  avgCookieSale: 3.7
+
+
+}
+
+for (var i = 0; i <= 13; i = i + 1){
+  var Paris = numofCust (20, 38);
+  console.log(storesHourArr[i],sales(Paris, 2.3));
+    storesHourArr[i], sales(Paris, 2.3)
+
+var Paris = {
+  miniCrust: 20,
+  maxCrust: 38,
+  avgCookieSale: 2.3,
+
+
+}
+for (var i = 0; i <= 13; i = i + 1){
+  var Lima = numofCust (2, 16,);
+  console.log(storesHourArr[i],sales(Lima, 4.6));
+    storesHourArr[i], sales(Lima, 4.6)
+
+var Lima = {
+  miniCrust: 2,
+  maxCrust: 16,
+  avgCookieSale: 4.6,
+}
+
+
+  seattle.custPerHour();
+  console.log(sales(30, 6.3), custPerHour(23, 65))
+  // random num of customers
+
